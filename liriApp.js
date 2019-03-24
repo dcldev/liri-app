7cd04f52d827a435f7222ceaddbcba418ab619c6
Han Seoulo <cu.rogue.coder@gmail.com>
2019-03-24 03:41:53 -0400

    added comments and constants

--- liriApp.js
+++ liriApp.js
@@ -1,15 +1,20 @@
 require("dotenv").config();
 
+//Constants We Need to RUN the Application
+const axios = require('axios');
+const moment = require('moment');
+var fs = require("fs");
+
+//Spotify API requirements
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 
 var spotify = new Spotify(keys.spotify);
 
-spotify
-  .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
-  .then(function(data) {
-    console.log(data); 
-  })
-  .catch(function(err) {
-    console.error('Error occurred: ' + err); 
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+   
+  console.log(data.tracks.items); 
   });
